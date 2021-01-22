438479c034bc9e8e7144a84f04d781c9b60e72ce
Oksana Chorna <ksuchorna@gmail.com>
2021-01-23 01:08:49 +0200

    7_2 перероблено

--- task-7_2.js
+++ task-7_2.js
@@ -7,9 +7,19 @@ const ingredients = [
   "Приправы",
 ];
 
+// ingredients.forEach((ingredient) => {
+//   const ingredientsRef = document.querySelector("#ingredients");
+//   const itemRef = document.createElement("li");
+//   itemRef.textContent = ingredient;
+//   ingredientsRef.appendChild(itemRef);
+// });
+
+let ingredientsList = [];
+
 ingredients.forEach((ingredient) => {
-  const ingredientsRef = document.querySelector("#ingredients");
   const itemRef = document.createElement("li");
-  itemRef.textContent = ingredient;
-  ingredientsRef.appendChild(itemRef);
+  itemRef.innerHTML = ingredient;
+  ingredientsList.push(itemRef);
 });
+
+document.querySelector("#ingredients").append(...ingredientsList);
