cdc61d2767668bcf4f018e72f467b0536598a989
Oksana Chorna <ksuchorna@gmail.com>
2021-01-25 00:15:31 +0200

    Завдання 7_6 зроблене

--- task-7_6.js
+++ task-7_6.js
@@ -3,3 +3,19 @@
 // - Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
 //-  Если введено подходящее количество, то border инпута
 // становится зеленым, если неправильное - красным.
+
+const inputRef = document.querySelector("#validation-input");
+const dataLengthValue = document
+  .querySelector("input")
+  .getAttribute("data-length");
+
+inputRef.addEventListener("blur", (event) => {
+  console.log(inputRef.value.length, dataLengthValue);
+  if (inputRef.value.length >= dataLengthValue) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  }
+});
