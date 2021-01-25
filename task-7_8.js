c2d7b1c08ef043696b13137bc8ddb619ceb59695
Oksana Chorna <ksuchorna@gmail.com>
2021-01-25 21:37:07 +0200

    Пофіксила селектор

--- task-7_8.js
+++ task-7_8.js
@@ -1,21 +1,21 @@
-const controlsRef = document.querySelector("#controls");
 const btnRenderRef = document.querySelector('button[data-action = "render"]');
 const btnDestroyRef = document.querySelector('button[data-action = "destroy"]');
 const boxesRef = document.querySelector("#boxes");
-const inputRef = document.querySelector("input");
+const inputRef = document.querySelector("#controls input");
 
 btnRenderRef.addEventListener("click", getAmount);
 btnDestroyRef.addEventListener("click", destroyBoxes);
 
 function createBoxes(amount) {
-  inputRef.value = "";
   for (let i = 0; i <= amount - 1; i++) {
     const divRef = document.createElement("div");
     divRef.style.backgroundColor = getRandomColor();
     divRef.style.width = 30 + i * 10 + "px";
     divRef.style.height = 30 + i * 10 + "px";
+    console.log(divRef);
     boxesRef.appendChild(divRef);
   }
+  inputRef.value = "";
 }
 
 function getAmount(event) {
