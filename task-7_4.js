080d68bd6f717d3f5ed8b7bbf3b065b5f3a79c92
Oksana Chorna <ksuchorna@gmail.com>
2021-01-28 00:11:09 +0200

    7_4 переоблено

--- task-7_4.js
+++ task-7_4.js
@@ -3,18 +3,12 @@ const valueRef = document.querySelector("#value");
 const incrementRef = document.querySelector('button[data-action="increment"]');
 const decrementRef = document.querySelector('button[data-action="decrement"]');
 
-function increment() {
-  return function (event) {
-    counterValue += 1;
-    valueRef.textContent = counterValue;
-  };
-}
+incrementRef.addEventListener("click", (e) => {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+});
 
-function decrement() {
-  return function (event) {
-    counterValue -= 1;
-    valueRef.textContent = counterValue;
-  };
-}
-incrementRef.addEventListener("click", increment());
-decrementRef.addEventListener("click", decrement());
+decrementRef.addEventListener("click", (e) => {
+  counterValue -= 1;
+  valueRef.textContent = counterValue;
+});
